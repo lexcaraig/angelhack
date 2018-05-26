8327e45a5523df2fc4d9a186bd580ad9c0a94162
Trish Moreno <trishmoreno02@gmail.com>
2018-05-26 18:21:45 +0800

    created category cards

--- angelhack-web/src/app/shared/components/components.module.ts
+++ angelhack-web/src/app/shared/components/components.module.ts
@@ -2,7 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MaterialModule } from '../material/material.module';
+<<<<<<< HEAD
 import { RouterModule } from '@angular/router';
+=======
+import { CategoryComponent } from './category/category.component';
+>>>>>>> created category cards
 
 @NgModule({
   imports: [
@@ -11,7 +15,7 @@ import { RouterModule } from '@angular/router';
 
     MaterialModule
   ],
-  declarations: [ToolbarComponent],
-  exports: [ToolbarComponent]
+  declarations: [ToolbarComponent, CategoryComponent],
+  exports: [ToolbarComponent, CategoryComponent]
 })
 export class ComponentsModule { }
