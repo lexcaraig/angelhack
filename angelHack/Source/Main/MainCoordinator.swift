2064e67260a6d80a77f7dafab0236490ae65f3bb
alvin joseph valdez <alvinjosephvaldez@Macintoshs-MacBook-Pro-3.local>
2018-05-26 12:45:31 +0800

    ignore commit

---
+++ angelHack/Source/Main/MainCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  MainCoordinator.swift
+//  angelHack
+//
+//  Created by alvin joseph valdez on 26/05/2018.
+//  Copyright © 2018 alvin joseph valdez. All rights reserved.
+//
+
+import Foundation
---
+++ angelHack/Source/Main/MainView.swift
@@ -0,0 +1,9 @@
+//
+//  MainView.swift
+//  angelHack
+//
+//  Created by alvin joseph valdez on 26/05/2018.
+//  Copyright © 2018 alvin joseph valdez. All rights reserved.
+//
+
+import Foundation
