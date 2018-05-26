a51d0c60ca4071114e15a8984a2d01d63f86317a
Lex Caraig <lcaraig@exist.com>
2018-05-26 19:14:39 +0800

    [web] fix merge conflict

--- angelhack-web/src/app/shared/components/components.module.ts
+++ angelhack-web/src/app/shared/components/components.module.ts
@@ -2,11 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MaterialModule } from '../material/material.module';
-<<<<<<< HEAD
 import { RouterModule } from '@angular/router';
-=======
 import { CategoryComponent } from './category/category.component';
->>>>>>> created category cards
 
 @NgModule({
   imports: [
--- angelhack-web/src/app/shared/firebase/firebase.module.ts
+++ angelhack-web/src/app/shared/firebase/firebase.module.ts
@@ -6,14 +6,14 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../../../environments/environment';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 
 @NgModule({
   imports: [
     CommonModule,
-
+    AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebase, 'angel-hack-buddies'), // imports firebase/app needed for everything
-    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
     AngularFireStorageModule // imports firebase/storage only needed for storage features
   ],
